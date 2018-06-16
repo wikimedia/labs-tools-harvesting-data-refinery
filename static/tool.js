dc8cb58875df9c22614f3e02684decd6efe08bf3
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-16 19:19:04 +0200

    Typo

--- static/tool.js
+++ static/tool.js
@@ -1,7 +1,7 @@
 function loadValues() {
     $.getJSON('api-to-review', {
         property: $('input[name="property"]').val(),
-        username: $('input[name="username"]').val(),
+        user: $('input[name="username"]').val(),
     }).then(function(data) {
         console.log(data);
         
