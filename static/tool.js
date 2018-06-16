cda699dceccc374292f3898617e0baf580564158
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-16 19:07:03 +0200

    Require username

--- static/tool.js
+++ static/tool.js
@@ -1,6 +1,7 @@
 function loadValues() {
     $.getJSON('api-to-review', {
-        property: $('#property').val()
+        property: $('#property').val(),
+        username: $('#username').val(),
     }).then(function(data) {
         console.log(data);
         
