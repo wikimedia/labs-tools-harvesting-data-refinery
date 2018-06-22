b6ad6e880666715905f99c9e2f79d4ccc99f85a0
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-22 15:42:30 +0200

    Document ready

--- static/tool.js
+++ static/tool.js
@@ -1,11 +1,13 @@
-$('#configuration').submit(function(event) {
-    event.preventDefault();
-    $.getJSON('api-to-review', {
-        property: $('input[name="property"]').val(),
-        user: $('input[name="username"]').val(),
-    }).then(function(data) {
-        for(var i = 0; i < data.length; i++) {
-            $('#values').append("<div>" + data[i].html + "</div>");
-        }
-    });
-})
+$( document ).ready(function() {
+    $('#configuration').submit(function(event) {
+        event.preventDefault();
+        $.getJSON('api-to-review', {
+            property: $('input[name="property"]').val(),
+            user: $('input[name="username"]').val(),
+        }).then(function(data) {
+            for(var i = 0; i < data.length; i++) {
+                $('#values').append("<div>" + data[i].html + "</div>");
+            }
+        });
+    })
+});
