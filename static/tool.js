53e061d94be7d23001bd1a3d542fcc35033b1895
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-22 15:52:14 +0200

    Empty HTML before refilling it from api

--- static/tool.js
+++ static/tool.js
@@ -5,11 +5,12 @@ $( document ).ready(function() {
             property: $('input[name="property"]').val(),
             user: $('input[name="username"]').val(),
         }).then(function(data) {
+            $('#values').html("");
             for(var i = 0; i < data.length; i++) {
                 $('#values').append('<div class="value" id="value-' + data[i].rev_id + '" data-rev-id="' + data[i].rev_id + '">' + data[i].html + '</div>');
             }
             $('.value').click(function() {
-		    console.log($( this ).attr('data-rev-id'));
+                console.log($( this ).attr('data-rev-id'));
             });
         });
     });
