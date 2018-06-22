067795bf3bfaa39a7847cf71b8a763af56eb70db
Martin Urbanec <martin.urbanec@wikimedia.cz>
2018-06-22 15:51:26 +0200

    Bind only once at a time

--- static/tool.js
+++ static/tool.js
@@ -6,8 +6,11 @@ $( document ).ready(function() {
             user: $('input[name="username"]').val(),
         }).then(function(data) {
             for(var i = 0; i < data.length; i++) {
-                $('#values').append("<div>" + data[i].html + "</div>");
+                $('#values').append('<div class="value" id="value-' + data[i].rev_id + '" data-rev-id="' + data[i].rev_id + '">' + data[i].html + '</div>');
             }
+            $('.value').click(function() {
+		    console.log($( this ).attr('data-rev-id'));
+            });
         });
-    })
+    });
 });
